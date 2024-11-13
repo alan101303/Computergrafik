f7c9f2fa0bc315a70a1a65f7a07473536ef50990
richieSaynode <richie.saseetharan@bluewin.ch>
2024-11-13 19:18:11 +0100

    replace with new assignment 8 files

--- assignment/src/main.cpp
+++ assignment/src/main.cpp
@@ -1,31 +1,44 @@
-#include "ShadowViewer.h"
+#include "lsystem.h"
 #include <iostream>
+#include <vector>
+#include "utils/render_svg.h"
 
-#ifdef _WIN32
-#  include <windows.h>
-#  include <stdlib.h>
-#  include <errhandlingapi.h>
-#endif
+int main(int arg_count, char *arg_values[]) {
+	if (arg_count < 2 || arg_count > 3) {
+        std::cout << "usage: lsys in_file_path [out_file_path]" << std::endl;
+		return 1;
+	}
 
-int main(int argc, char *argv[])
-{
-#ifdef _WIN32
-    // This make crashes very visible - without them, starting the
-    // application from cmd.exe or powershell can surprisingly hide
-    // any signs of a an application crash!
-    SetErrorMode(0);
-#endif
-    char const* mesh = "neutral.off";
-     if (argc > 2) {
-        std::cerr << "Usage: ShadowViewer mesh.off" << std::endl;
-        exit(1);
-     } else if (argc==2) {
-        mesh = argv[1];
-     }
+	std::string const in_file_path(arg_values[1]);
+	std::string const out_file_path = (arg_count == 3) ? arg_values[2] : "";
 
-    ShadowViewer window("Shadow Viewer", 640, 480);
+	auto lsys = LindenmayerSystem::load_lsystem_from_file(in_file_path);
 
-    std::string mesh_filename = std::string(MESH_PATH) + "/" + mesh;
-    window.loadMesh(mesh_filename);
-    return window.run();
+    if (!lsys) {
+		std::cout << "Failed to load system\n\n";
+		return 1;
+    }
+
+    if (!out_file_path.empty()) {
+		// If the name of the output file is specified, we write the expanded sequence and render to a file.
+
+		std::string const sym_sequence_expanded = lsys->expand();
+        write_string_to_file(sym_sequence_expanded, out_file_path + ".txt");
+
+		// Build lines according to symbols
+		std::vector<Segment> segments = lsys->draw(sym_sequence_expanded);
+
+		// Output segments to an SVG file
+		std::string const svg = draw_segments_to_svg_file(segments);
+		write_string_to_file(svg, out_file_path + ".svg");
+
+	} else {
+		// If an output name wasn't specified, we run the interactive mode.
+		std::cout << "- Interactive mode -" << std::endl;
+		render_glfw(*lsys);
+    }
+
+    std::cout << std::endl;
+
+    return 0;
 }
